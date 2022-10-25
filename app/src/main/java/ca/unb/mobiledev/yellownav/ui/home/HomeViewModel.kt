41c02424ad547d7905a7f895650e67f1e464ac25
HaydenShen <51305554+HaydenShen@users.noreply.github.com>
2022-10-24 22:35:56 -0300

    create prototype

--- app/src/main/java/ca/unb/mobiledev/yellownav/ui/home/HomeViewModel.kt
+++ app/src/main/java/ca/unb/mobiledev/yellownav/ui/home/HomeViewModel.kt
@@ -7,7 +7,16 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Nov 4:\n" +
+                "Bus#2 is delayed.\n" +
+                "\n" +
+                "Nov 20:\n" +
+                "Bus#6 is delayed.\n" +
+                "Bus#7 is delayed.\n" +
+                "\n" +
+                "Nov 24:\n" +
+                "Due to snow storm, all buses are delayed.\n"
+
     }
     val text: LiveData<String> = _text
 }
